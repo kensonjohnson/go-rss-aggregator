ea7f18acdc581660c01d034d214ade612c4aaa65
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-12-03 08:56:02 -0600

    Make healthcheck more useful

--- handler_readiness.go
+++ handler_readiness.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, struct{}{})
+	data := struct {
+		Status string `json:"status"`
+	}{
+		Status: "available",
+	}
+	respondWithJSON(w, 200, data)
 }
